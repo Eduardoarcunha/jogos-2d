f7b734a94de716e2953b077fc409e68d1d405224
Eduardo <eduardoarcunha12@gmail.com>
2023-10-19 11:25:57 -0300

    feat: add monsters

---
+++ UnnamedGame/Assets/Scripts/Bat/Bat.cs
@@ -0,0 +1,209 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject player;
+    public Transform attackPoint2;
+    public Transform attackPoint3;
+
+    public Transform raycastPoint;
+    public BarthaSzabolcs.Tutorial_SpriteFlash.SimpleFlash flashEffect;
+    public GameObject batProjectilePrefab;
+
+    [Header("Properties")]
+    private float speed = 3f;
+    private float chaseRange = 5f;
+    [SerializeField] private float attackPoint2Range = 0.5f;
+    private float life = 5;
+
+    [Header("Attack Settings")]
+    private int nextAttack = 1;
+    private float meleeAttackProbability = 0.7f;
+    private float meleeAttackRange = 0.8f;
+
+    [Header("Ranged Attack Settings")]
+    private float rangeAttackMinDis = 1.5f;
+    private float rangeAttackMaxDis = 4f;
+    private float batProjectileSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    private int playerMask;
+    private int gameObjectId;
+    private float signPlayerToBat;
+    private bool isAttacking = false;
+    private bool isDead = false;
+    private float attackCooldown = 1f;
+    private float attackCooldownRemaining = 0f;
+    private float attackRange = .8f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        playerMask = LayerMask.GetMask("Player");
+        gameObjectId = gameObject.GetInstanceID();
+
+        PlayerCombat.OnHitEnemyEvent += TakeDamage;
+    }
+
+    private void Start()
+    {
+        nextAttack = Random.value < meleeAttackProbability ? 2 : 3;
+    }
+
+    private void Update()
+    {
+        signPlayerToBat = Mathf.Sign(player.transform.position.x - transform.position.x);
+        if (!isDead) UpdateAnimator();
+
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovementAndAttacks();
+    }
+
+    private void HandleMovementAndAttacks()
+    {
+        if (!isAttacking && attackCooldownRemaining <= 0f && !isDead)
+        {
+
+            if (nextAttack == 2)
+            {
+                // Melee attack
+                if (Vector2.Distance(transform.position, player.transform.position) <= meleeAttackRange)
+                {
+                    StartMeleeAttack();
+                }
+                else
+                {
+                    rb.velocity = new Vector2(signPlayerToBat * speed, rb.velocity.y);
+                }
+            }
+            else if (nextAttack == 3)
+            {
+                // Ranged attack
+                if (Vector2.Distance(transform.position, player.transform.position) < rangeAttackMinDis)
+                {
+                    rb.velocity = new Vector2(-signPlayerToBat * speed, rb.velocity.y);
+                }
+                else if (Vector2.Distance(transform.position, player.transform.position) > rangeAttackMaxDis)
+                {
+                    rb.velocity = new Vector2(signPlayerToBat * speed, rb.velocity.y);
+                }
+                else
+                {
+                    StartRangedAttack();
+                }
+            }
+        }
+
+        attackCooldownRemaining -= Time.fixedDeltaTime;
+    }
+
+
+
+    private void StartRangedAttack()
+    {
+        rb.velocity = Vector2.zero;
+        isAttacking = true;
+        animator.SetTrigger("Attack3Trigger");
+        nextAttack = Random.value < meleeAttackProbability ? 2 : 3;
+    }
+
+
+    private bool IsPlayerWithinRangedAttackRange()
+    {
+        return Vector2.Distance(transform.position, player.transform.position) <= attackRange;
+    }
+
+    private void StartMeleeAttack()
+    {
+        rb.velocity = Vector2.zero;
+        isAttacking = true;
+        animator.SetTrigger("Attack2Trigger");
+        nextAttack = Random.value < meleeAttackProbability ? 2 : 3;
+    }
+
+    private void UpdateAnimator()
+    {
+        transform.localScale = new Vector3(signPlayerToBat * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnHitAttack2()
+    {
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(attackPoint3.position, attackPoint2Range, playerMask);
+        foreach (Collider2D obj in hitObjects)
+        {
+            obj.GetComponent<PlayerCombat>().Hitted(transform, 1);
+        }
+    }
+
+    private void OnHitAttack3()
+    {
+        GameObject batProjectile = Instantiate(batProjectilePrefab, attackPoint2.position, Quaternion.identity);
+        batProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(batProjectileSpeed * Mathf.Sign(transform.localScale.x), 0);
+    }
+
+    private void OnEndAttack2()
+    {
+        attackCooldownRemaining = attackCooldown;
+        isAttacking = false;
+    }
+
+    private void OnEndAttack3()
+    {
+        Debug.Log("OnEndAttack3");
+        attackCooldownRemaining = attackCooldown;
+        isAttacking = false;
+    }
+
+
+    private void TakeDamage(int id, int damage)
+    {
+        if (id == gameObjectId){
+            life -= damage;
+            flashEffect.Flash();
+            if (life <= 0)
+            {
+                OnDeath();
+            }
+        }
+        return;
+    }
+
+    private void OnDeath()
+    {
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
+        boxCollider.enabled = false;
+        isDead = true;
+        animator.SetTrigger("DeathTrigger");
+    }
+
+    private void DestroyBat()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerCombat.OnHitEnemyEvent -= TakeDamage;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint2.position, attackPoint2Range);
+        Gizmos.DrawRay(raycastPoint.position, new Vector2(signPlayerToBat, 0) * 10f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
---
+++ UnnamedGame/Assets/Scripts/Goblin/Goblin.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject player;
+    public Transform attackPoint1;
+    public Transform attackPoint2;
+    public Transform raycastPoint;
+    public BarthaSzabolcs.Tutorial_SpriteFlash.SimpleFlash flashEffect;
+    public GameObject bombPrefab;
+
+    [Header("Properties")]
+    private float speed = 3.5f;
+    private float chaseRange = 5f;
+    private float attackPoint1Range = 0.5f;
+    private float attackPoint2Range = 0.5f;
+    private float life = 4;
+    private int attack1Damage = 2;
+
+    [Header("Attack Settings")]
+    private int nextAttack = 1;
+    private float meleeAttackProbability = 0.5f;
+    private float meleeAttackRange = 0.8f;
+
+    [Header("Ranged Attack Settings")]
+    private float rangeAttackMinDis = 2f;
+    private float rangeAttackMaxDis = 6f;
+    private float bombSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    private int playerMask;
+    private int gameObjectId;
+    private float signPlayerToGoblin;
+    private bool isAttacking = false;
+    private bool isDead = false;
+    private float attackCooldown = 1.5f;
+    private float attackCooldownRemaining = 0f;
+    private float attackRange = .8f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        playerMask = LayerMask.GetMask("Player");
+        gameObjectId = gameObject.GetInstanceID();
+
+        PlayerCombat.OnHitEnemyEvent += TakeDamage;
+    }
+
+    private void Start()
+    {
+        nextAttack = Random.value < meleeAttackProbability ? 1 : 2;
+    }
+
+    private void Update()
+    {
+        signPlayerToGoblin = Mathf.Sign(player.transform.position.x - transform.position.x);
+        if (!isDead) UpdateAnimator();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovementAndAttacks();
+    }
+
+    private void HandleMovementAndAttacks()
+    {
+        if (!isAttacking && attackCooldownRemaining <= 0f && !isDead)
+        {
+
+            if (nextAttack == 1)
+            {
+                // Melee attack
+                if (Vector2.Distance(transform.position, player.transform.position) <= meleeAttackRange)
+                {
+                    StartMeleeAttack();
+                }
+                else
+                {
+                    rb.velocity = new Vector2(signPlayerToGoblin * speed, rb.velocity.y);
+                }
+            }
+            else if (nextAttack == 2)
+            {
+                // Ranged attack
+                if (Vector2.Distance(transform.position, player.transform.position) < rangeAttackMinDis)
+                {
+                    rb.velocity = new Vector2(-signPlayerToGoblin * speed, rb.velocity.y);
+                }
+                else if (Vector2.Distance(transform.position, player.transform.position) > rangeAttackMaxDis)
+                {
+                    rb.velocity = new Vector2(signPlayerToGoblin * speed, rb.velocity.y);
+                }
+                else
+                {
+                    StartRangedAttack();
+                }
+            }
+        }
+
+        attackCooldownRemaining -= Time.fixedDeltaTime;
+    }
+
+
+
+    private void StartRangedAttack()
+    {
+        rb.velocity = Vector2.zero;
+        isAttacking = true;
+        animator.SetTrigger("Attack2Trigger");
+        nextAttack = Random.value < meleeAttackProbability ? 1 : 2;
+    }
+
+
+    private bool IsPlayerWithinRangedAttackRange()
+    {
+        return Vector2.Distance(transform.position, player.transform.position) <= attackRange;
+    }
+
+    private void StartMeleeAttack()
+    {
+        rb.velocity = Vector2.zero;
+        isAttacking = true;
+        animator.SetTrigger("Attack1Trigger");
+        nextAttack = Random.value < meleeAttackProbability ? 1 : 2;
+    }
+
+    private void UpdateAnimator()
+    {
+        transform.localScale = new Vector3(signPlayerToGoblin * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        animator.SetBool("IsMoving", rb.velocity.x != 0);
+    }
+
+    private void OnHitAttack1()
+    {
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(attackPoint1.position, attackPoint1Range, playerMask);
+        foreach (Collider2D obj in hitObjects)
+        {
+            obj.GetComponent<PlayerCombat>().Hitted(transform, attack1Damage);
+        }
+    }
+
+    private void OnEndAttack1()
+    {
+        attackCooldownRemaining = attackCooldown;
+        isAttacking = false;
+    }
+
+    private void OnEndAttack2()
+    {
+        attackCooldownRemaining = attackCooldown;
+        GameObject bomb = Instantiate(bombPrefab, attackPoint1.position, Quaternion.identity);
+        bomb.GetComponent<Rigidbody2D>().velocity = new Vector2(bombSpeed * Mathf.Sign(transform.localScale.x), 0);
+        isAttacking = false;
+    }
+
+
+    private void TakeDamage(int id, int damage)
+    {
+        if (id == gameObjectId){
+            life -= damage;
+            flashEffect.Flash();
+            if (life <= 0)
+            {
+                OnDeath();
+            }
+        }
+        return;
+    }
+
+    private void OnDeath()
+    {
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
+        boxCollider.enabled = false;
+        isDead = true;
+        animator.SetTrigger("DeathTrigger");
+    }
+
+    private void DestroyGoblin()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerCombat.OnHitEnemyEvent -= TakeDamage;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint1.position, attackPoint1Range);
+        Gizmos.DrawWireSphere(attackPoint2.position, attackPoint2Range);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
